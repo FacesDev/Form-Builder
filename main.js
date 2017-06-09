1cd4619fff5c5feb9b696d727486bba207fd0c79
Chris <kitecabrinha@gmail.com>
2017-06-09 10:08:01 -0400

    Second commit, finsished select

--- main.js
+++ main.js
@@ -1,31 +1,39 @@
 var title = document.createElement("H1");
-title.textContent = 'Sign up for my Web App'
-console.log([title]);
-var container = document.querySelector("#container");
-container.appendChild(title)
-
-
-
-
-// var formElement = document.querySelector("#container");
+title.textContent = 'Sign up for my Web App';
+container.appendChild(title);
 
 for (var i = 0; i <formData.length; i++) {
-
   var control = formData[i];
   if (control.type === "textarea") {
     var inputElement = document.createElement('textarea');
-
   }else if (control.type === "select") {
     var inputElement = document.createElement('select');
+    for (var j = 0; j < control.options.length; j++) {
 
-  }
+      var optionElement = document.createElement('option');
+
+      optionElement.label = formData[i].options[j].label;
+
+      optionElement.value = formData[i].options[j].value;
+
+      inputElement.appendChild(optionElement);
+}
+}
+
   else {
     var inputElement = document.createElement('input');
+  }
+inputElement.placeholder = control.label;
+
+inputElement.id = control.id;
+inputElement.type = control.type;
+ container.appendChild(inputElement);
 
   }
+var bottomDiv = document.createElement("div");
+bottomDiv.id = "bottomDiv";
+container.appendChild(bottomDiv);
 
-  inputElement.id = control.id;
-  inputElement.type = control.type;
-  inputElement.placeholder = control.label;
-  container.appendChild(inputElement);
-  }
+var submit = document.createElement("input");
+submit.setAttribute("type", "submit");
+bottomDiv.appendChild(submit);
